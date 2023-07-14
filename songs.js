ae59cf5834396be405fd230ca643398c6bb2a49b
Riltter <135280966+Riltter@users.noreply.github.com>
2023-07-14 11:47:55 -0300

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        src: "./src/musica1mp3.mp3",
+        name: "Stairway - Patrick Patrikios"
+    },
+    {
+        src: "./src/musica2mp3.mp3",
+        name: "Osc Project - Reflecting Echoes"
+    },
+    {
+        src: "./src/musica3mp3.mp3",
+        name: "How's it Supposed to feel - Neffex"
+    }
+]
